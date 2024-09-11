b2d2ca62e9310eee42ab347bb6e53351e5d10129
Mansib Talukder <nabil99002@gmail.com>
2024-09-10 22:59:39 -0700

    Removed all magic numbers in Main class. Replaced them with constants.

--- src/code/ca/bcit/comp2522/bank/Main.java
+++ src/code/ca/bcit/comp2522/bank/Main.java
@@ -2,122 +2,124 @@ package ca.bcit.comp2522.bank;
 
 public class Main {
 
+    // Constants for Albert Einstein
+    private static final Name NAME_1 = new Name ("Albert", "Einstein");
+    private static final Date BIRTH_1 = new Date (14, 3, 1879);
+    private static final Date DEATH_1 = new Date (18, 4, 1955);
+    private static final String ACCOUNT_1 = "abc123";
+    private static final Date ACC_OPEN_1 = new Date(1, 1, 1900);
+    private static final Date ACC_CLOSE_1 = new Date(14, 10, 1950);
+    private static final int PIN_1 = 3141;
+    private static final double BALANCE_1 = 1000;
+    private static final double WITHDRAW_1 = 100;
+
+    // Constants for Nelson Mandela
+    private static final Name NAME_2 = new Name ("Nelson", "Mandela");
+    private static final Date BIRTH_2 = new Date (18, 7, 1918);
+    private static final Date DEATH_2 = new Date (5, 12, 2013);
+    private static final String ACCOUNT_2 = "654321";
+    private static final Date ACC_OPEN_2 = new Date(10, 5, 1994);
+    private static final int PIN_2 = 4664;
+    private static final double BALANCE_2 = 2000;
+    private static final double WITHDRAW_2 = 200;
+
+    // Constants for Frida Kahlo
+    private static final Name NAME_3 = new Name ("Frida", "Kahlo");
+    private static final Date BIRTH_3 = new Date (6, 7, 1907);
+    private static final Date DEATH_3 = new Date (6, 7, 1907);
+    private static final String ACCOUNT_3 = "frd123";
+    private static final Date ACC_OPEN_3 = new Date(1, 1, 1940);
+    private static final Date ACC_CLOSE_3 = new Date(13, 7, 1954);
+    private static final int PIN_3 = 1907;
+    private static final double BALANCE_3 = 500;
+    private static final double WITHDRAW_3 = 50;
+
+    // Constants for Jackie Chan
+    private static final Name NAME_4 = new Name ("Jackie", "Chan");
+    private static final Date BIRTH_4= new Date (7, 4, 1954);
+    private static final String ACCOUNT_4 = "chan789";
+    private static final Date ACC_OPEN_4 = new Date(1, 10, 1980);
+    private static final int PIN_4 = 1954;
+    private static final double BALANCE_4 = 3000;
+    private static final double WITHDRAW_4 = 500;
+
+
     public static void main(final String[] args){
 
         // Details of Albert Einstein
-        Name name1 = new Name ("Albert", "Einstein");
-        Date birth1 = new Date (14, 3, 1879);
-        Date death1 = new Date (18, 4, 1955);
-        Person person1 = new Person(name1, birth1, death1);
+        Person person1 = new Person(NAME_1, BIRTH_1, DEATH_1);
 
         System.out.println("Person 1's details: ");
-        System.out.println("Initials: " + name1.getInitials());
-        System.out.println("Full Name: " + name1.getFullName());
-        System.out.println("Name Reversed: " + name1.getReverseName());
+        System.out.println("Initials: " + NAME_1.getInitials());
+        System.out.println("Full Name: " + NAME_1.getFullName());
+        System.out.println("Name Reversed: " + NAME_1.getReverseName());
         System.out.println("Details: " + person1.getDetails());
 
-        String acc1 = "abc123";
-        Date open1 = new Date(1, 1, 1900);
-        Date close1 = new Date(14, 10, 1950);
-
-        BankClient client1 = new BankClient(person1, open1, acc1);
+        BankClient client1 = new BankClient(person1, ACC_OPEN_1, ACCOUNT_1);
 
         System.out.println("Bank Client details: " + client1.getDetails());
 
-        int pin1 = 3141;
-        double balance1 = 1000;
-        double withdraw1 = 100;
-
-        BankAccount bank1 = new BankAccount(acc1, open1, close1, person1, balance1);
-        bank1.withdraw(withdraw1, pin1);
+        BankAccount bank1 = new BankAccount(ACCOUNT_1, ACC_OPEN_1, ACC_CLOSE_1, person1, BALANCE_1);
+        bank1.withdraw(WITHDRAW_1, PIN_1);
 
         System.out.println("His bank status: " + bank1.getDetails());
         System.out.println();
 
-
         // Details of Nelson Mandela
-        Name name2 = new Name ("Nelson", "Mandela");
-        Date birth2 = new Date (18, 7, 1918);
-        Date death2 = new Date (5, 12, 2013);
-        Person person2 = new Person(name2, birth2, death2);
+        Person person2 = new Person(NAME_2, BIRTH_2, DEATH_2);
 
         System.out.println("Person 2's details: ");
-        System.out.println("Initials: " + name2.getInitials());
-        System.out.println("Full Name: " + name2.getFullName());
-        System.out.println("Name Reversed: " + name2.getReverseName());
+        System.out.println("Initials: " + NAME_2.getInitials());
+        System.out.println("Full Name: " + NAME_2.getFullName());
+        System.out.println("Name Reversed: " + NAME_2.getReverseName());
         System.out.println("Details: " + person2.getDetails());
 
-        String acc2 = "654321";
-        Date open2 = new Date(10, 5, 1994);
-
-        BankClient client2 = new BankClient(person2, open2, acc2);
+        BankClient client2 = new BankClient(person2, ACC_OPEN_2, ACCOUNT_2);
 
         System.out.println("Bank Client details: " + client2.getDetails());
 
-        int pin2 = 4664;
-        double balance2 = 2000;
-        double withdraw2 = 200;
-
-        BankAccount bank2 = new BankAccount(acc2, open2, person2, balance2);
-        bank2.withdraw(withdraw2, pin2);
+        BankAccount bank2 = new BankAccount(ACCOUNT_2, ACC_OPEN_2, person2, BALANCE_2);
+        bank2.withdraw(WITHDRAW_2, PIN_2);
 
         System.out.println("His bank status: " + bank2.getDetails());
         System.out.println();
 
 
         // Details of Frida Kahlo
-        Name name3 = new Name ("Frida", "Kahlo");
-        Date birth3 = new Date (6, 7, 1907);
-        Date death3 = new Date (13, 7, 1954);
-        Person person3 = new Person(name3, birth3, death3);
+        Person person3 = new Person(NAME_3, BIRTH_3, DEATH_3);
 
         System.out.println("Person 3's details: ");
-        System.out.println("Initials: " + name3.getInitials());
-        System.out.println("Full Name: " + name3.getFullName());
-        System.out.println("Name Reversed: " + name3.getReverseName());
+        System.out.println("Initials: " + NAME_3.getInitials());
+        System.out.println("Full Name: " + NAME_3.getFullName());
+        System.out.println("Name Reversed: " + NAME_3.getReverseName());
         System.out.println("Details: " + person3.getDetails());
 
-        String acc3 = "frd123";
-        Date open3 = new Date(1, 1, 1940);
-        Date close3 = new Date(13, 7, 1954);
-        BankClient client3 = new BankClient(person3, open3, acc3);
+        BankClient client3 = new BankClient(person3, ACC_OPEN_3, ACCOUNT_3);
 
         System.out.println("Bank Client details: " + client3.getDetails());
 
-        int pin3 = 1907;
-        double balance3 = 500;
-        double withdraw3 = 50;
-
-        BankAccount bank3 = new BankAccount(acc3, open3, close3, person3, balance3);
-        bank3.withdraw(withdraw3, pin3);
+        BankAccount bank3 = new BankAccount(ACCOUNT_3, ACC_OPEN_3, ACC_CLOSE_3, person3, BALANCE_3);
+        bank3.withdraw(WITHDRAW_3, PIN_3);
 
         System.out.println("His bank status: " + bank3.getDetails());
         System.out.println();
 
 
         // Details of Jackie Chan
-        Name name4 = new Name ("Jackie", "Chan");
-        Date birth4 = new Date (7, 4, 1954);
-        Person person4 = new Person(name4, birth4);
+        Person person4 = new Person(NAME_4, BIRTH_4);
 
         System.out.println("Person 4's details: ");
-        System.out.println("Initials: " + name4.getInitials());
-        System.out.println("Full Name: " + name4.getFullName());
-        System.out.println("Name Reversed: " + name4.getReverseName());
+        System.out.println("Initials: " + NAME_4.getInitials());
+        System.out.println("Full Name: " + NAME_4.getFullName());
+        System.out.println("Name Reversed: " + NAME_4.getReverseName());
         System.out.println("Details: " + person4.getDetails());
 
-        String acc4 = "chan789";
-        Date open4 = new Date(1, 10, 1980);
-        BankClient client4 = new BankClient(person4, open4, acc4);
+        BankClient client4 = new BankClient(person4, ACC_OPEN_4, ACCOUNT_4);
 
         System.out.println("Bank Client details: " + client4.getDetails());
 
-        int pin4 = 1954;
-        double balance4 = 3000;
-        double withdraw4 = 500;
-
-        BankAccount bank4 = new BankAccount(acc4, open4, person4, balance4);
-        bank4.withdraw(withdraw4, pin4);
+        BankAccount bank4 = new BankAccount(ACCOUNT_4, ACC_OPEN_4, person4, BALANCE_4);
+        bank4.withdraw(WITHDRAW_4, PIN_4);
 
         System.out.println("His bank status: " + bank4.getDetails());
         System.out.println();
